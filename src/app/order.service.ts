6652c4abeb4683d108a4cc214f006efe7b4b3ed1
Manuel Colorado <manuel1412@gmail.com>
2018-10-07 22:42:43 +0100

    completed error handling and http.get

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Order } from './order';
 import { ORDERS } from './mock-orders';
@@ -9,11 +11,46 @@ import { ORDERS } from './mock-orders';
   providedIn: 'root'
 })
 export class OrderService {
+  private apiUrl = 'api/orders';  // URL to web api TODO
 
-  constructor(private messageService : MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+  /** GET heroes from the server */
   getOrders(): Observable<Order[]> {
-    this.messageService.add('[LOG] OrderService: fetched orders');
-    return of(ORDERS);
+    this.log('fetched orders');
+    return this.http.get<Order[]>(this.apiUrl)
+      .pipe(
+        tap(orders => this.log('fetched orders')),
+        catchError(this.handleError('getOrders', []))
+      );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`[LOG] OrderService: ${message}`);
+  }
+
+
+  /**
+  * From Angular.io documentation
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
